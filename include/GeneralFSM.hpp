e70a0e9cf6a2c182d2ed24b9ed940194b6affe54
LiZe <l1015178747@outlook.com>
2023-01-17 00:18:05 +0800

    1

---
+++ include/GeneralFSM.hpp
@@ -0,0 +1,14 @@
+#pragma once
+/*
+    本文件为通用状态机头文件，意在于便于对哨兵的状态机的开发与调试
+    主要在于便于添加状态，添加状态函数等
+    在开发最后阶段可能会进行更进一步的封装，剔除init过程
+*/
+
+
+#include <vector>
+
+namespace GFSM
+{
+    
+} // namespace FSM
